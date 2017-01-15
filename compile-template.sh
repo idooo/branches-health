b84a0916e0231b89217768f33e783ce9e4f8191d
idooo <alex.shteinikov@gmail.com>
2017-01-15 11:50:42 +1000

    (improvement) better "compiler" for the template

--- compile-template.sh
+++ compile-template.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DATA=$(cat ./assets/index.html)
+DATA=$(cat ./assets/index.html | tr "\n" " " |tr -s " ")
 
 echo "package core" > ./core/template.go
 echo "const IndexTemplate = \`$DATA\`" >> ./core/template.go
